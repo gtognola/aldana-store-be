701b2ef249f54c725f54020b87604ef411689866
ftogno92 <tognofacundo0@gmail.com>
2024-07-24 01:34:24 -0300

    Product CRUD

--- src/services/product.service.js
+++ src/services/product.service.js
@@ -1,31 +1,70 @@
-const PRODUCTS = [
+/*const PRODUCTS = [
 	{ id: 1, name: "Product 1", price: 100 },
 	{ id: 2, name: "Product 2", price: 200 },
 	{ id: 3, name: "Product 3", price: 300 },
 	{ id: 4, name: "Product 4", price: 400 },
 	{ id: 5, name: "Product 5", price: 500 },
 ];
+*/
+const express = require('express');
+const router = express.Router();
+const db = require('../config/conn')
 
-getAllProducts = () => {
-	return PRODUCTS;
+// obtener todos los productos
+getAllProducts = (req, res) => {
+	const sql = 'SELECT * FROM Product';
+	db.query(sql, (err, results)=>{
+		if (err) throw err;
+		res.json(results);
+	});
 };
 
-getProductById = (id) => {
-	return PRODUCTS.find((product) => product.id == id);
+// obtener el producto mediante el id
+getProductById = (req, res) => {
+	const sql = 'SELECT * FROM Product WHERE id = ?';
+	db.query(sql, [req.params.id], (err, result)=>{
+		if (err) throw err;
+		res.json(result);
+	});
+};
+// Creación de un nuevo producto
+createProduct = (req, res) => {
+	const sql = 'INSERT INTO Product SET ?';
+	const newProduct = {
+		name : req.body.name,
+		decription: req.body.description,
+		price: req.body.price,
+		image: req.body.imagen,
+		categoriId: req.body.categoriId
+	};
+	db.query(sql, newProduct, (err, result)=> {
+		if (err) throw err;
+		res.json({ id: result.insertId, ...newProduct});
+	});
 };
 
-createProduct = (product) => {
-	PRODUCTS.push(product);
+//Actualizar un producto
+updateProduct = (req, res) => {
+	const sql = 'UPDATE Product SER ? WHERE id = ?';
+	const updateProduct = {
+		name: req.body.name,
+		description: req.body.description,
+		price: req.body.price,
+		image: req.body.image,
+		categoryId: req.body.categoryId
+	};
+	db.query(sql, [updatedProduct, req.params.id], (err, result) => {
+		if (err) throw err;
+		res.json(updatedProduct);
+	  });
 };
 
-updateProduct = (id, product) => {
-	const index = PRODUCTS.findIndex((product) => product.id == id);
-	PRODUCTS[index] = product;
-};
-
-deleteProduct = (id) => {
-	const index = PRODUCTS.findIndex((product) => product.id == id);
-	PRODUCTS.splice(index, 1);
+deleteProduct = (req, res) => {
+	const sql = 'DELETE FROM Product WHERE id = ?';
+	db.query(sql, [req.params.id], (err, result) => {
+	  if (err) throw err;
+	  res.json({ message: 'Producto eliminado' });
+	});
 };
 
 module.exports = {
