c3cb7ce17e272eb4766f29b0542377a199b12bd8
ftogno92 <tognofacundo0@gmail.com>
2024-09-08 20:14:27 -0300

    getOrderById + try and catch on functions

--- src/services/order.service.js
+++ src/services/order.service.js
@@ -1,21 +1,54 @@
 const { db } = require("../config/conn");
 
 // crear pedido
-const createOrder = async (newOder) => {
-	const sql = "INSERT INTO `Order` SET ?";
-	const [result] = await db.query(sql, newOder);
-
-	return { id: result.insertId, ...newOder };
+const createOrder = async (newOrder) => {
+  try {
+    const sql = "INSERT INTO `Order` SET ?";
+    const [result] = await db.query(sql, newOrder);
+    return { id: result.insertId, ...newOrder };
+  } catch (error) {
+    console.error("Error al crear la orden:", error);
+    throw error;
+  }
 };
 
 const addProductToOrder = async (newOrderProduct) => {
-	const sql = "INSERT INTO OrderProduct SET ?";
-	const [result] = await db.query(sql, newOrderProduct);
+  try {
+    const sql = "INSERT INTO OrderProduct SET ?";
+    const [result] = await db.query(sql, newOrderProduct);
 
-	return { id: result.insertId, ...newOrderProduct };
+    return { id: result.insertId, ...newOrderProduct };
+  } catch {
+    console.log("Error al obtener las ordenes", error);
+    throw error;
+  }
 };
 
+const getOrderById = async (orderId) => {
+  try {
+    const sql = "SELECT * FROM `Order` WHERE id = ?";
+    const [rows] = await db.query(sql, [orderId]);
+    return rows.length > 0 ? rows[0] : null;
+  } catch (error) {
+    console.error(`Error al obtener la orden con ID ${orderId}:`, error);
+    throw error;
+  }
+};
+const getAllOrders = async () => {
+    try {
+        const sql = "SELECT * FROM `Order`";
+        const [rows] = await db.query(sql);
+        return rows;
+    } catch (error) {
+        console.error('Error al obtener las órdenes:', error);
+        throw error;
+    }
+};
+
+
 module.exports = {
-	createOrder,
-	addProductToOrder,
+  createOrder,
+  addProductToOrder,
+  getOrderById,
+  getAllOrders,
 };
